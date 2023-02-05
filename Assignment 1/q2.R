883645874e7953f64fa80f384fa92c21d68b2795
Vishan Summinga Sonagadu <94801273+vishans@users.noreply.github.com>
2023-02-05 17:16:17 -0500

    Finish question 2

---
+++ Assignment 1/q2.R
@@ -0,0 +1,24 @@
+secant<-function(f,x0,x1=x0+0.001,eps=1e-8) {
+  fx0<-f(x0)
+  fx1<-f(x1)
+  
+  while (TRUE) {
+    xstar<-x1-fx1*(x1-x0)/(fx1-fx0)
+    fxstar<-f(xstar)
+    
+    if (abs(fxstar) < eps) {
+      break
+    }
+    
+    x0<-x1
+    x1<-xstar
+    fx0<-fx1
+    fx1<-fxstar
+  }
+  
+  xstar
+}
+
+myFunction <- function(x) {(1 / (1 + exp(-(x-1)))) - 0.5}
+
+print(secant(myFunction, 0.9, 1.1))

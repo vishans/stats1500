00d4bfbf76f8aa0a6c26f165133d37c0295b96c3
Vishan Summinga Sonagadu <94801273+vishans@users.noreply.github.com>
2023-02-22 16:50:37 -0500

    Paractically finish question 2 of 2 of Assignment 2

---
+++ Assignment 2/q2.R
@@ -0,0 +1,25 @@
+# (a)
+f <- function(x, y, p){
+  stopifnot('P has to be in the range (-1, 1)' = (p > -1 && p < 1))
+  
+  (1/(2 * pi * sqrt(1 - p^2)))* exp((-1/(2*(1 - p^2))) * (x^2 - 2*p*x*y + y^2))
+}
+
+# (b)
+x <- seq(-3,3, length.out = 100)
+y <- seq(-3,3, length.out = 100)
+z <- outer(x,y, f, 0)
+persp(x,y,z, col = 'lightblue', phi = 30, theta = 30, zlab = 'f(x,y)')
+
+# (c)
+for(i in seq(0,100,10)){
+  persp(x,y,z, col = 'lightblue', phi = i, theta = i, zlab = 'f(x,y)')
+  Sys.sleep(1)
+}
+
+# (d)
+for(j in c(-0.8,-0.5,0.0,0.5,0.8)){
+  z <- outer(x,y, f, j)
+  persp(x,y,z, col = 'lightblue', phi = 45, theta = 30, zlab = paste('p = ', j))
+  Sys.sleep(1)
+}
